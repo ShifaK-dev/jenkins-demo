958bbadc95e7ceb275464d14d78652b356a6be8b
Shifa K <shifa.kunjumon.sh@gmail.com>
2026-07-31 16:25:54 +0000

    Initial Spring Boot project

---
+++ src/main/java/com/shifa/jenkins_demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.shifa.jenkins_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Jenkins CI/CD on AWS!";
+    }
+}
